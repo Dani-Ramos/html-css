cfecd0814900ea8709f5edb10140c3e7e5d33983
Dani-Ramos <danielsonramosandrade5@gmail.com>
2026-05-24 22:44:49 -0100

    novo

---
+++ www/php/javascript/script.js
@@ -0,0 +1,19 @@
+const mode = document.getElementById('mode_icon')
+const form = document.getElementById('form_login')
+
+function mudar() {
+
+    if (mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon')
+        mode.classList.add('fa-sun')
+
+        form.classList.add('dark')
+
+        return;
+    }
+    
+    mode.classList.add('fa-moon')
+    mode.classList.remove('fa-sun')
+    form.classList.remove('dark')
+}
+
